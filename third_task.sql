aad4543c7c98c55ff75c397e8cd79582fcf7d8f0
Egorka <trololo221@bk.ru>
2025-05-14 21:21:19 +0300

    Add query for the third task

--- third_task.sql
+++ third_task.sql
@@ -18,4 +18,30 @@ SELECT d.dwarf_id   AS dwarf_id,
                                  FROM dwarf_equipment de
                                  WHERE de.dwarf_id = d.dwarf_id)
            )        AS related_entities
-FROM Dwarves AS d
+FROM Dwarves AS d;
+
+
+--запрос для получения информации о мастерской, включая идентификаторы
+--назначенных ремесленников, текущих проектов, используемых и производимых ресурсов
+
+SELECT w.workshop_id AS workshop_id,
+       w.name        AS name,
+       w.type        AS type,
+       w.quality     AS quality,
+       json_object(
+               'craftsdwarf_ids', (SELECT json_arrayagg(wc.dwarf_id)
+                                   FROM workshop_craftsdwarves AS wc
+                                   WHERE wc.workshop_id = w.workshop_id),
+               'project_ids', (SELECT json_arrayagg(p.project_id)
+                               FROM projects AS p
+                               WHERE p.workshop_id = w.workshop_id),
+               'input_material_ids', (SELECT json_arrayagg(wm.material_id)
+                                      FROM workshop_materials AS wm
+                                      WHERE wm.workshop_id = w.workshop_id
+                                        AND is_input),
+               'output_product_ids', (SELECT json_arrayagg(wm.material_id)
+                                      FROM workshop_materials AS wm
+                                      WHERE wm.workshop_id = w.workshop_id
+                                        AND NOT is_input)
+           )         AS related_entities
+FROM Workshops AS w
